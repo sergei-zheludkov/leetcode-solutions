ad5c1e032b025d36543c97f2d5c18b940229b770
sergozheludkov <sergozheludkov@yandex.ru>
2024-02-02 08:44:14 +0300

    initial commit + 5 resolved problems

---
+++ src/1-(easy)-two_sum.ts
@@ -0,0 +1,37 @@
+const test1_arr = [2,7,11,15];
+const test1_target = 9;
+const test2_arr = [3,2,4];
+const test2_target = 6;
+const test3_arr = [3,3];
+const test3_target = 6;
+
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    const hasComplement = map.has(complement);
+
+    if (!hasComplement) {
+      map.set(nums[i], i);
+
+      continue;
+    }
+
+    const element = map.get(complement);
+
+    if (element !== undefined) {
+      return [element, i];
+    }
+
+  }
+
+  return [];
+}
+
+console.log('test_1: => [0,1]', twoSum(test1_arr, test1_target));
+console.log('test_2: => [1,2]', twoSum(test2_arr, test2_target));
+console.log('test_3: => [0,1]', twoSum(test3_arr, test3_target));
+
+export { twoSum };
---
+++ src/9-(easy)-palindrome_number.ts
@@ -0,0 +1,22 @@
+function isPalindrome(num: number): boolean {
+  for (let step = 0, str = String(num); step < (str.length / 2); step++) {
+    if (!(str[step] === str[str.length - (step + 1)])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log('test1: true =>', isPalindrome(0));
+console.log('test1: true =>', isPalindrome(121));
+console.log('test1: true =>', isPalindrome(11));
+console.log('test1: true =>', isPalindrome(1221));
+console.log('test1: false =>', isPalindrome(-123));
+console.log('test1: false =>', isPalindrome(10));
+console.log('test1: false =>', isPalindrome(104));
+console.log('test1: false =>', isPalindrome(-121));
+console.log('test1: false =>', isPalindrome(123));
+
+
+export { isPalindrome };

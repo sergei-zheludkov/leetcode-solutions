a4b897c95c1dcc4920e7763669833d53d8e7eede
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-02-10 11:51:41 +0300

    problem(647): palindromic substrings (#20)

---
+++ src/104-(easy)-maximum_depth_of_binary_tree.ts
@@ -0,0 +1,17 @@
+// link to problem: https://leetcode.com/problems/maximum-depth-of-binary-tree
+
+import { TreeNode } from "./models/tree-node";
+
+const test1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+const test2 = new TreeNode(1, null, new TreeNode(2));
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+
+console.log(`test 1: \n Expected: 3 \n Output: ${maxDepth(test1)}`);
+console.log(`test 2: \n Expected: 2 \n Output: ${maxDepth(test2)}`);

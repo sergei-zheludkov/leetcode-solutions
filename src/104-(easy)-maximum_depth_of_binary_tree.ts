abd97b32ce9f6f5d86fac667292aadae7c1275fd
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-02-12 17:48:30 +0300

    problem(110): balanced binary tree (#33)

--- src/104-(easy)-maximum_depth_of_binary_tree.ts
+++ src/104-(easy)-maximum_depth_of_binary_tree.ts
@@ -2,8 +2,8 @@
 
 import { TreeNode } from "./models/tree-node";
 
-const test1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-const test2 = new TreeNode(1, null, new TreeNode(2));
+const test1_list = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+const test2_list = new TreeNode(1, null, new TreeNode(2));
 
 function maxDepth(root: TreeNode | null): number {
   if (!root) {
@@ -13,5 +13,7 @@ function maxDepth(root: TreeNode | null): number {
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
 
-console.log(`test 1: \n Expected: 3 \n Output: ${maxDepth(test1)}`);
-console.log(`test 2: \n Expected: 2 \n Output: ${maxDepth(test2)}`);
+console.log(`test 1: \n Expected: 3 \n Output: ${maxDepth(test1_list)}`);
+console.log(`test 2: \n Expected: 2 \n Output: ${maxDepth(test2_list)}`);
+
+export { maxDepth };

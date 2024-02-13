c68678ac45d7dd319809ece352647b24783fad8b
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-02-13 13:09:56 +0300

    problem(19): remove nth node from end of list (#39)

---
+++ src/helpers/get-list-length.ts
@@ -0,0 +1,9 @@
+import { ListNode } from "../models/list-node";
+
+export const getListLength = (head: ListNode | null): number => {
+  if (!head) {
+    return 0;
+  }
+
+  return 1 + getListLength(head.next);
+};

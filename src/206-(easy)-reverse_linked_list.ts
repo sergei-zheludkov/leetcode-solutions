6c2fed057c82fde3484f33bd0c7f6859459ebab1
sergozheludkov <sergozheludkov@yandex.ru>
2024-02-13 10:21:49 +0300

    problem(24): swap nodes in pairs

--- src/206-(easy)-reverse_linked_list.ts
+++ src/206-(easy)-reverse_linked_list.ts
@@ -5,18 +5,6 @@ import { getArrayFromList } from "./helpers/get-array-from-list";
 
 const test1_list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number 6
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
 function reverseList(head: ListNode | null): ListNode | null {
   let prev: ListNode | null = null;
   let current = head;

f48641f3a82c38b8ed870a950b31e241985a7542
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-02-13 06:15:53 +0300

    problem(2108): find first palindromic string in the array (#34)

---
+++ src/2108-(easy)-find_first_palindromic_string_in_the_array.ts
@@ -0,0 +1,39 @@
+// link to problem: https://leetcode.com/problems/find-first-palindromic-string-in-the-array
+
+const test1_words = ["abc","car","ada","racecar","cool"];
+const test2_words = ["notapalindrome","racecar"];
+const test3_words = ["def","ghi"];
+
+function firstPalindrome(words: string[]): string {
+  for (const word of words) {
+    const reversed = word.split('').reverse().join('');
+
+    if (reversed === word) {
+      return word;
+    }
+  }
+
+  return "";
+}
+
+// function firstPalindrome(words: string[]): string {
+//   for (const word of words) {
+//     let reversed = "";
+//
+//     for (let index = 0; index < word.length; index++) {
+//       reversed = `${word[index]}${reversed}`;
+//     }
+//
+//     if (reversed === word) {
+//       return word;
+//     }
+//   }
+//
+//   return "";
+// }
+
+console.log(`test1: \n EXPECTED: ada \n OUTPUT:`, firstPalindrome(test1_words));
+console.log(`test2: \n EXPECTED: racecar \n OUTPUT:`, firstPalindrome(test2_words));
+console.log(`test3: \n EXPECTED: "" \n OUTPUT:`, firstPalindrome(test3_words));
+
+export { firstPalindrome };

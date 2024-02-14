00c0ee594f397f0eb2f42670ab6555a93e09f35f
sergozheludkov <sergozheludkov@yandex.ru>
2024-02-14 14:09:21 +0300

    problem(25): reverse nodes in k group

---
+++ src/helpers/concat-lists.ts
@@ -0,0 +1,11 @@
+import { ListNode } from "../models/list-node";
+
+export const concatLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
+  if (!list1) {
+    return list2;
+  }
+
+  list1.next = concatLists(list1.next, list2);
+
+  return list1;
+}

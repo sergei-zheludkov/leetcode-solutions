5816db3bf994a214c00ecc6b34b5df7fd27c0e1f
sergozheludkov <sergozheludkov@yandex.ru>
2024-02-15 10:09:30 +0300

    problem(2703): return length of arguments passed

--- src/119-(easy)-pascals_triangle_II.ts
+++ src/119-(easy)-pascals_triangle_II.ts
@@ -5,16 +5,13 @@ const test2_row_index = 0;
 const test3_row_index = 1;
 
 function getRow(numRows: number): number[] {
-  if (numRows === 0) {
-    return [1];
-  }
+  const newRow: number[] = [1];
 
-  if (numRows === 1) {
-    return [1,1];
+  if (numRows === 0) {
+    return newRow;
   }
 
   const lastRow = getRow(numRows - 1);
-  const newRow: number[] = [1];
 
   for (let index = 0; index < lastRow.length; index++) {
     const current = lastRow[index];

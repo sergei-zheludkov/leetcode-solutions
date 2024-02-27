0f1e69cbd798f3032a85eb9644952e199d37aa52
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-02-27 06:23:07 +0300

    problem(543): diameter of binary tree (#66)

---
+++ src/543-[easy](diameter-of-binary-tree)/diameter-of-binary-tree.test.ts
@@ -0,0 +1,29 @@
+import { TreeNode } from "../models/tree-node";
+import { diameterOfBinaryTree } from "./diameter-of-binary-tree";
+
+const test1_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+const test3_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+const test4_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(8), new TreeNode(9)), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+const test5_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(8), new TreeNode(9)), new TreeNode(5, new TreeNode(10), new TreeNode(11))), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+const test6_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(5, new TreeNode(6), new TreeNode(7)))), new TreeNode(3));
+const test7_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(6))), new TreeNode(3, new TreeNode(5, new TreeNode(7))));
+const test8_tree = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(8), new TreeNode(9)), new TreeNode(5, new TreeNode(10))), new TreeNode(3, new TreeNode(6, new TreeNode(11)), new TreeNode(7)));
+const test9_tree = new TreeNode(1);
+const test10_tree = new TreeNode(1, new TreeNode(2));
+
+describe(('diameter-of-binary-tree'), () => {
+  test('has width', () => {
+    expect(diameterOfBinaryTree(test1_tree)).toBe(3);
+    expect(diameterOfBinaryTree(test3_tree)).toBe(4);
+    expect(diameterOfBinaryTree(test4_tree)).toBe(5);
+    expect(diameterOfBinaryTree(test5_tree)).toBe(5);
+    expect(diameterOfBinaryTree(test6_tree)).toBe(5);
+    expect(diameterOfBinaryTree(test7_tree)).toBe(6);
+    expect(diameterOfBinaryTree(test8_tree)).toBe(6);
+  });
+
+  test('thin cases', () => {
+    expect(diameterOfBinaryTree(test9_tree)).toBe(0);
+    expect(diameterOfBinaryTree(test10_tree)).toBe(1);
+  });
+})
---
+++ src/543-[easy](diameter-of-binary-tree)/diameter-of-binary-tree.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from "../models/tree-node";
+
+export const diameterOfBinaryTree = (root: TreeNode | null): number => {
+  let max = 0;
+
+  const maxDepth = (node: TreeNode | null): number => {
+    if (!node) {
+      return 0;
+    }
+
+    const leftMax = maxDepth(node.left);
+    const rightMax = maxDepth(node.right);
+
+    max = Math.max(max, leftMax + rightMax);
+
+    return 1 + Math.max(leftMax, rightMax);
+  };
+
+  maxDepth(root);
+
+  return max;
+};

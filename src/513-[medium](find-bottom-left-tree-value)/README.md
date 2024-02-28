5ac8f66b784c79f80d63c8a5bc9722c533296b66
sergozheludkov <sergozheludkov@yandex.ru>
2024-02-28 05:21:20 +0300

    problem(513): find bottom left tree value

---
+++ src/513-[medium](find-bottom-left-tree-value)/README.md
@@ -0,0 +1,1 @@
+link to problem: https://leetcode.com/problems/find-bottom-left-tree-value
---
+++ src/513-[medium](find-bottom-left-tree-value)/find-bottom-left-tree-value.ts
@@ -0,0 +1,50 @@
+import { TreeNode } from '../models/tree-node';
+
+export const findBottomLeftValue = (root: TreeNode | null): number => {
+  if (!root) {
+    return 0;
+  }
+
+  let maxLvl = 0;
+  let val = root.val;
+
+  const dfs = (tree: TreeNode | null, level: number): void => {
+    if (!tree) {
+      return;
+    }
+
+    if (maxLvl < level) {
+      maxLvl = level;
+      val = tree.val;
+    }
+
+    dfs(tree.left, level + 1);
+    dfs(tree.right, level + 1);
+  };
+
+  dfs(root, maxLvl);
+
+  return val;
+};
+
+// export const findBottomLeftValue = (root: TreeNode | null): number => {
+//   const collection = new Map<number, number[]>();
+//
+//   const dfs = (tree: TreeNode | null, level: number = 0): void => {
+//     if (!tree) {
+//       return;
+//     }
+//
+//     const item = collection.get(level);
+//     collection.set(level, item ? [...item, tree.val] : [tree.val]);
+//
+//     dfs(tree.left, level + 1);
+//     dfs(tree.right, level + 1);
+//   };
+//
+//   dfs(root);
+//
+//   const [, [val]] = Array.from(collection.entries())[collection.size - 1];
+//
+//   return val;
+// };

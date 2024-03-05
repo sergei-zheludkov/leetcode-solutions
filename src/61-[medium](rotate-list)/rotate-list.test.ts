086af67394242930a6e8e5d7101e41d13e2c0cd2
sergozheludkov <sergozheludkov@yandex.ru>
2024-03-05 10:21:50 +0300

    problem(61): rotate list | second solution

--- src/61-[medium](rotate-list)/rotate-list.test.ts
+++ src/61-[medium](rotate-list)/rotate-list.test.ts
@@ -1,18 +1,39 @@
-import { getArrayFromList } from '../helpers/get-array-from-list';
 import { createLinkedList } from '../helpers/create-linked-list';
 import { rotateRight } from './rotate-list';
 
 test.each([
   {
-    list: createLinkedList([1,2,3,4,5]),
+    nodes: [1],
+    k: 0,
+    expected: [1],
+  },
+  {
+    nodes: [1,2],
+    k: 0,
+    expected: [1,2],
+  },
+  {
+    nodes: [1,2],
+    k: 2,
+    expected: [1,2],
+  },
+  {
+    nodes: [1,2,3,4,5],
     k: 2,
     expected: [4,5,1,2,3],
   },
   {
-    list: createLinkedList([0,1,2]),
+    nodes: [0,1,2],
     k: 4,
     expected: [2,0,1],
   },
-])('rotate list', ({ list, k, expected }) => {
-  expect(getArrayFromList(rotateRight(list, k))).toEqual(expected);
+  {
+    nodes: [1,2,3],
+    k: 2000000000,
+    expected: [2,3,1],
+  },
+])('rotate list', ({ nodes, k, expected }) => {
+  const list = createLinkedList(nodes);
+
+  expect(rotateRight(list, k)).toEqual(createLinkedList(expected));
 });
--- src/61-[medium](rotate-list)/rotate-list.ts
+++ src/61-[medium](rotate-list)/rotate-list.ts
@@ -1,19 +1,54 @@
 import { ListNode } from '../models/list-node';
-import { getArrayFromList } from '../helpers/get-array-from-list';
-import { createLinkedList } from '../helpers/create-linked-list';
+import { getListLength } from '../helpers/get-list-length';
+// import { createLinkedList } from "../helpers/create-linked-list";
+// import { getArrayFromList } from "../helpers/get-array-from-list";
 
 export const rotateRight = (head: ListNode | null, k: number): ListNode | null => {
-  if (!head) {
+  if (!head || !head.next || !k) {
     return head;
   }
 
-  const array = getArrayFromList(head);
-  
-  while (k) {
-    const last = array.pop() ?? NaN;
-    array.unshift(last);
-    k--;
+  let length = getListLength(head);
+  k %= length;
+
+  if (!k) {
+    return head;
   }
-  
-  return createLinkedList(array);
+
+  let dummy: ListNode | null = new ListNode(0, head);
+  let list: ListNode | null = null;
+
+  while (dummy) {
+    if (!dummy.next) {
+      dummy.next = head;
+      break;
+    }
+
+    if (length === k) {
+      list = dummy.next;
+      dummy.next = null;
+      dummy = list;
+    } else {
+      dummy = dummy.next;
+    }
+    length--;
+  }
+
+  return list;
 };
+
+// const rotateRight = (head: ListNode | null, k: number): ListNode | null => {
+//   if (!head) {
+//     return head;
+//   }
+//
+//   const array = getArrayFromList(head);
+//
+//   while (k) {
+//     const last = array.pop() ?? NaN;
+//     array.unshift(last);
+//     k--;
+//   }
+//
+//   return createLinkedList(array);
+// };

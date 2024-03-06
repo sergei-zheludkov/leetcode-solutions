7b9c6070a5e122ea3e60759e6750f5bb8460a5db
sergozheludkov <sergozheludkov@yandex.ru>
2024-03-06 19:00:16 +0300

    problem(2625): flatten deeply nested array

---
+++ src/2625-[medium](flatten-deeply-nested-array)/flatten-deeply-nested-array.ts
@@ -0,0 +1,21 @@
+type MultiDimensionalArray = (number | MultiDimensionalArray)[];
+
+export const flat = (arr:  MultiDimensionalArray, n: number): MultiDimensionalArray => {
+  return arr.reduce<MultiDimensionalArray>((acc, elem) => {
+    if (Array.isArray(elem) && n) {
+      acc.push(...flat(elem, n - 1));
+    } else {
+      acc.push(elem);
+    }
+    return acc;
+  }, []);
+};
+
+// export const flat = (arr:  MultiDimensionalArray, n: number): MultiDimensionalArray => {
+//   return arr.reduce<MultiDimensionalArray>((acc, elem) => {
+//     if (Array.isArray(elem) && n) {
+//       return [...acc, ...flat(elem, n - 1)];
+//     }
+//     return [...acc, elem];
+//   }, []);
+// };

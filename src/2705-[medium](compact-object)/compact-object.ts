898eef747e117e9656289ec9cc588ee7203229fb
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-03-20 05:13:41 +0300

    problem(2705): compact object (#114)

---
+++ src/2705-[medium](compact-object)/compact-object.ts
@@ -0,0 +1,40 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+export const compactObject = (obj: Obj): Obj => {
+  if (Array.isArray(obj)) {
+    const result: Obj = [];
+
+    for (const item of obj) {
+      if (!item) {
+        continue;
+      }
+
+      if (typeof item === 'object') {
+        result.push(compactObject(item));
+      } else {
+        result.push(item);
+      }
+    }
+
+    return result;
+  }
+
+  const result: Obj = {};
+
+  for (const key in obj) {
+    const item = obj[key];
+
+    if (!item) {
+      continue;
+    }
+
+    if (typeof item === 'object') {
+      result[key] = compactObject(item);
+    } else {
+      result[key] = item;
+    }
+  }
+
+  return result;
+};

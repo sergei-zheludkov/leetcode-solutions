8455115708aa2e4fd92db8a87bb48bedbfa32236
sergozheludkov <sergozheludkov@yandex.ru>
2024-03-26 11:10:25 +0300

    problem(102): binary tree level order traversal

--- src/222-[easy](count-complete-tree-nodes)/count-complete-tree-nodes.test.ts
+++ src/222-[easy](count-complete-tree-nodes)/count-complete-tree-nodes.test.ts
@@ -1,14 +1,19 @@
 import { countNodes } from './count-complete-tree-nodes';
-import { createBinaryTree } from '../helpers/create-binary-tree';
+import { TreeNode } from '../models/tree-node';
+
+const tree1 = null;
+const tree2 = new TreeNode(1);
+const tree3 = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6)));
+const tree4 =  new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(8)), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+const tree5 = new TreeNode(1, new TreeNode(2, new TreeNode(4, new TreeNode(8), new TreeNode(9)), new TreeNode(5, new TreeNode(10))), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+
 
 test.each([
-  { nodes: [], expected: 0 },
-  { nodes: [1], expected: 1 },
-  { nodes: [1,2,3,4,5,6], expected: 6 },
-  { nodes: [1,2,3,4,5,6,7,8], expected: 8 },
-  { nodes: [1,2,3,4,5,6,7,8,9,10], expected: 10 },
-])('count complete tree nodes', ({ nodes, expected }) => {
-  const tree = createBinaryTree(nodes);
-
+  { tree: tree1, expected: 0 },
+  { tree: tree2, expected: 1 },
+  { tree: tree3, expected: 6 },
+  { tree: tree4, expected: 8 },
+  { tree: tree5, expected: 10 },
+])('count complete tree nodes', ({ tree, expected }) => {
   expect(countNodes(tree)).toBe(expected);
 });

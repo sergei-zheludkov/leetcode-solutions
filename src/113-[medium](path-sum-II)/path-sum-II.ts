88905be2c5f22065b37fa970393f849a57479175
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-04-17 06:45:55 +0300

    problem(113): path sum II (#178)

---
+++ src/113-[medium](path-sum-II)/path-sum-II.ts
@@ -0,0 +1,51 @@
+import { TreeNode } from '../models/tree-node';
+
+export const pathSum = (root: TreeNode | null, targetSum: number): number[][] => {
+  const result: number[][] = []; 
+  
+  const dfs = (head: TreeNode | null, currentSum = targetSum, path: number[] = []) => {
+    if (!head) {
+      return;
+    }
+
+    currentSum -= head.val;
+    path = [...path, head.val];
+
+    if (!head.left && !head.right && !currentSum) {
+      result.push(path);
+      return;
+    }
+
+    dfs(head.left, currentSum, path);
+    dfs(head.right, currentSum, path);
+  };
+  
+  dfs(root);
+
+  return result;
+};
+
+// const pathSum = (root: TreeNode | null, targetSum: number): number[][] => {
+//   const result: number[][] = [];
+//
+//   const dfs = (head: TreeNode | null, currentSum = targetSum, path: string = '') => {
+//     if (!head) {
+//       return;
+//     }
+//
+//     currentSum -= head.val;
+//     path = `${path}|${head.val}`;
+//
+//     if (!head.left && !head.right && !currentSum) {
+//       result.push(path.split('|').slice(1).map(Number));
+//       return;
+//     }
+//
+//     dfs(head.left, currentSum, path);
+//     dfs(head.right, currentSum, path);
+//   };
+//
+//   dfs(root);
+//
+//   return result;
+// };

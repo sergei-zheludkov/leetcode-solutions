5ccff03addf99e190d09eb7f810e2a1962d9d9fe
sergozheludkov <sergozheludkov@yandex.ru>
2024-04-30 08:42:30 +0300

    problem(1493): longest subarray of 1s after deleting one element

--- src/1481-[medium](least-number-of-unique-integers-after-k-removals)/least-number-of-unique-integers-after-k-removals.test.ts
+++ src/1481-[medium](least-number-of-unique-integers-after-k-removals)/least-number-of-unique-integers-after-k-removals.test.ts
@@ -1,10 +1,12 @@
 import { findLeastNumOfUniqueInts } from './least-number-of-unique-integers-after-k-removals';
 
-test('least number of unique integers after k removals', () => {
-  expect(findLeastNumOfUniqueInts([5, 5, 4], 1)).toBe(1);
-  expect(findLeastNumOfUniqueInts([4, 3, 1, 1, 3, 3, 2], 3)).toBe(2);
-  expect(findLeastNumOfUniqueInts([5,5,4,5,1,2,2,3,3,4,6,1,9], 3)).toBe(5);
-  expect(findLeastNumOfUniqueInts([5,5,4,5,1,2,2,3,3,4,6,1,9], 4)).toBe(4);
-  expect(findLeastNumOfUniqueInts([5,5,4,5,1,2,2,3,3,4,6,1,9], 5)).toBe(4);
-  expect(findLeastNumOfUniqueInts([5,5,4,5,1,2,2,3,3,4,6,1,9], 6)).toBe(3);
+test.each([
+  { nums: [5, 5, 4], k: 1, expected: 1 },
+  { nums: [4, 3, 1, 1, 3, 3, 2], k: 3, expected: 2 },
+  { nums: [5,5,4,5,1,2,2,3,3,4,6,1,9], k: 3, expected: 5 },
+  { nums: [5,5,4,5,1,2,2,3,3,4,6,1,9], k: 4, expected: 4 },
+  { nums: [5,5,4,5,1,2,2,3,3,4,6,1,9], k: 5, expected: 4 },
+  { nums: [5,5,4,5,1,2,2,3,3,4,6,1,9], k: 6, expected: 3 },
+])('least number of unique integers after k removals', ({ nums, k, expected }) => {
+  expect(findLeastNumOfUniqueInts(nums, k)).toBe(expected);
 });

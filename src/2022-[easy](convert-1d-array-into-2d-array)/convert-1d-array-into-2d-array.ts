16d170b56ba3c3072ff3725cba38a331866a8cd6
Sergei Zheludkov <sergei.zheludkov@apliteni.com>
2024-09-01 13:36:46 +0700

    problem(2022): convert 1d array into 2d array

---
+++ src/2022-[easy](convert-1d-array-into-2d-array)/convert-1d-array-into-2d-array.ts
@@ -0,0 +1,13 @@
+export const construct2DArray = (original: number[], m: number, n: number): number[][] => {
+  if (original.length !== m * n) {
+    return [];
+  }
+
+  const result: number[][] = [];
+
+  for (let i = 0; i < original.length; i += n) {
+    result.push(original.slice(i, i + n));
+  }
+
+  return result;
+};

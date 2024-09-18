ae705cc4189217a62155508ff563dd4373e7b652
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-09-18 13:01:08 +0700

    problem(3217): delete nodes from linked list present in array (#229)

---
+++ src/3217-[medium](delete-nodes-from-linked-list-present-in-array)/delete-nodes-from-linked-list-present-in-array.ts
@@ -0,0 +1,33 @@
+import { ListNode } from '../models/list-node';
+
+export const modifiedList = (nums: number[], head: ListNode | null): ListNode | null => {
+  const collection = new Set(nums);
+  const dummy = new ListNode(0, head);
+  let current = dummy;
+
+  while (current.next) {
+    if (collection.has(current.next.val)) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return dummy.next;
+};
+
+// export const modifiedList = (nums: number[], head: ListNode | null): ListNode | null => {
+//   const collection = new Set(nums);
+//
+//   if (!head) {
+//     return null;
+//   }
+//
+//   if (collection.has(head.val)) {
+//     return modifiedList(nums, head.next);
+//   }
+//
+//   head.next = modifiedList(nums, head.next);
+//
+//   return head;
+// };

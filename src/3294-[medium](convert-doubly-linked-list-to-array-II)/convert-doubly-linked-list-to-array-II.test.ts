d25142529043e2eac177c28befbbc72b85caceec
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-12-08 09:29:50 +0700

    problem(3294): convert doubly linked list to array II (#277)

---
+++ src/3294-[medium](convert-doubly-linked-list-to-array-II)/convert-doubly-linked-list-to-array-II.test.ts
@@ -0,0 +1,13 @@
+import { createSinglyLinkedList } from '../helpers/create-doubly-linked-list';
+import { toArray } from './convert-doubly-linked-list-to-array-II';
+
+test.each([
+  { nodes: [1,2,3,4,3,2,1] },
+  { nodes: [1,1,2,3,4,4,5,5] },
+  { nodes: [2,2,2,2,2] },
+  { nodes: [3,2,3,2,3,2] },
+])('convert doubly linked list to array II', ({ nodes }) => {
+  const list = createSinglyLinkedList(nodes);
+
+  expect(toArray(list)).toEqual(nodes);
+});

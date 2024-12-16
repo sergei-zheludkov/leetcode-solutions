df04305f92d447075e188ac29adbdecb78ea2cb0
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2024-12-16 20:09:00 +0700

    problem(328): odd even linked list (#266)

---
+++ src/328-[medium](odd-even-linked-list)/README.md
@@ -0,0 +1,1 @@
+link to problem: https://leetcode.com/problems/odd-even-linked-list
---
+++ src/328-[medium](odd-even-linked-list)/odd-even-linked-list.ts
@@ -0,0 +1,88 @@
+import { ListNode } from '../models/list-node';
+
+export const oddEvenList = (head: ListNode | null): ListNode | null => {
+  if (!head) {
+    return null;
+  }
+
+  let odd = head, even = head.next;
+
+  const evenHead = even;
+
+  while (even?.next) {
+    odd.next = even.next;
+    odd = odd.next;
+
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
+};
+
+
+// const oddEvenList = (head: ListNode | null): ListNode | null => {
+//   if (!head?.next?.next) {
+//     return head;
+//   }
+//
+//   const evenHead: ListNode | null = head.next;
+//   let odd: ListNode | null = head;
+//   let even: ListNode | null = evenHead;
+//
+//   while (even?.next) {
+//     odd.next = even.next;
+//     odd = odd.next;
+//
+//     even.next = odd.next;
+//     even = even.next;
+//   }
+//
+//   odd.next = evenHead;
+//
+//   return head;
+// };
+
+
+// export const oddEvenList = (head: ListNode | null): ListNode | null => {
+//   if (!head?.next?.next) {
+//     return head;
+//   }
+//
+//   const odd: number[] = [];
+//   const even: number[] = [];
+//
+//   let current: ListNode | null = head;
+//   let flag = true;
+//
+//   while (current) {
+//     if (flag) {
+//       odd.push(current.val);
+//     } else {
+//       even.push(current.val);
+//     }
+//
+//     flag = !flag;
+//     current = current.next;
+//   }
+//
+//   const result = new ListNode(0);
+//
+//   current = result;
+//
+//   for (const num of odd) {
+//     current.next = new ListNode(num);
+//
+//     current = current?.next;
+//   }
+//
+//   for (const num of even) {
+//     current.next = new ListNode(num);
+//
+//     current = current?.next;
+//   }
+//
+//   return result.next;
+// };
